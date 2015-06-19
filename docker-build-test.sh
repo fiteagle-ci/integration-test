b6005210f8c90d412fa474e7f6ffe52249918783
Björn Riemer <bjoern.riemer@tu-berlin.de>
2015-06-19 17:58:22 +0200

    fixed build-test-docker

--- docker-build-test.sh
+++ docker-build-test.sh
@@ -13,4 +13,4 @@ if [ "$1" = "-r" ]; then
 	runcmd "docker build --tag=ft2-buildtest --rm $(pwd)/build-test-docker/" || exit
 fi
 
-runcmd "docker run --rm -it -v \"${_CACHE_DIR}/:/root/.m2/\" ft2-buildtest"
+runcmd "docker run --rm -it -v ${_CACHE_DIR}/:/root/.m2/ ft2-buildtest"
